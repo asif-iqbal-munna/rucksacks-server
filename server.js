0fcc616f93bca80632728bfb8a531a4b61130825
asif-iqbal-munna <asifiqbalmunna1318@gmail.com>
2021-11-11 16:35:52 +0600

    changes

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const run = async () => {
       res.send(products);
     });
 
-    // Get A Single Product
+    // Get A Single Product jkfs
     app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
